da5b9fb7a2e4658ec7f11bac4d13fb2389eaa2d6
RunqiZhao <69561314+RunqiZhao@users.noreply.github.com>
2020-11-11 11:26:26 -0500

    format

--- MappingMoreFEMA/app.R
+++ MappingMoreFEMA/app.R
@@ -1,9 +1,12 @@
 library(shiny)
+library(shinydashboard)
 library(knitr)
 library(tidyverse)
 library(magrittr)
 library(drat)
 library(maps)
+library(ggplot2)
+library(dplyr)
 library(rgdal)
 library(geojsonio)
 library(tigris)
@@ -12,101 +15,119 @@ library(leaflet)
 library(grid)
 library(kableExtra)
 library(gridExtra)
-library(shinydashboard)
 library(lubridate)
 
 hrcc <- read.csv("hrc_county.csv", head = TRUE)
-hrcctable <- hrcc[,c(1:3,7,9:12)]
-colnames(hrcctable) <-  c("DisasterNum","DeclarationYear","Incident","State","County","ProjectAmount","FederalShareObligated","ToatlObligated")
-date <- gsub("T"," ", hrcc$obligatedDate)
-date <- gsub(".000Z", "", date)
-date <- ymd_hms(date)
+hrcc$obligatedDate <-  as_datetime(hrcc$obligatedDate)
+hrcc$obligatedYear <-  year(hrcc$obligatedDate)
+hrcctable <- hrcc[,c(1:3,7,9:12,15)]
+colnames(hrcctable) <-  c("DisasterNum","Declaration_Year","Incident","State","County","ProjectAmount","FederalShareObligated","ToatlObligated","Obligated_Year")
+
 
 loadJ <- geojsonio::geojson_read("gz_2010_us_050_00_5m.json", what = "sp")
 loadJ$GEO_ID <- loadJ$GEO_ID %>% substr(start = 10, stop = 14)
 
-hrcctable$ObligatedYear <- year(date)
-hrcc$obligatedYear <- year(date)
 
 ui <- dashboardPage(
-        dashboardHeader(title="FEMA Mapping"),
+        dashboardHeader(title="MappingFEMA"),
         dashboardSidebar(
             sidebarMenu(
-                menuItem("Table", tabName="Table"),
-                menuItem("Map", tabName="Map")
+            menuItem("Table", tabName="Table"),
+            menuItem("Map", tabName="Map")
             )
         ),
+        
         dashboardBody(
-            tabItems(
-                tabItem("Table",
-                        fluidRow(
+          tabItems(
+            tabItem("Table",
+                         fluidRow(
+                           column(4,
+                                  sliderInput("Year",
+                                              "Declaration Year:",
+                                              min=2009, max=2018, value=c(2009, 2018)),
+                                  sliderInput("Year3",
+                                              "Obligated Year:",
+                                              min=2009, max=2018, value=c(min(hrcctable$Obligated_Year), max(hrcctable$Obligated_Year)))
+                           ), 
+                           
+                           column(4,
+                                    selectInput("DisasterNum",
+                                                "DisasterNum: ",
+                                                c("All",
+                                                  unique(hrcctable$DisasterNum)))
+                             ),
+                             
                             column(4,
-                                   sliderInput("Year1", "Declaration Year: ", min=2010, max=2017, value=c(2010, 2017), sep=""),
-                                   sliderInput("Year2", "Obligated Year: ", min=2010, max=2017, value=c(2010, 2017), sep="")
-                            ),
-                            
-                            column(4,
-                                   selectInput("DisasterNum",
-                                               "Disaster Number: ",
-                                               c("All",
-                                                 unique(hrcctable$DisasterNum))),
-                                   selectInput("Incident1",
-                                               "Incident Type:",
-                                               c("All",
-                                                 unique(hrcctable$Incident)))
-                            ),
-                            column(4,
-                                   selectInput("State",
-                                               "State:",
-                                               c("All",
-                                                 unique(hrcctable$State))),
-                                   selectInput("County",
-                                               "County:",
-                                               c("All",
-                                                 unique(hrcctable$County)))
-                            ),
-                        ),
-                        DT::dataTableOutput("table"),style = "overflow-x: scroll;"
-                        ),
+                                    selectInput("Incident",
+                                                "Incident Type:",
+                                                c("All",
+                                                  unique(hrcctable$Incident)))
+                             ), 
+                             column(4,
+                                    selectInput("State",
+                                                "State:",
+                                                c("All",
+                                                  unique(hrcctable$State)))
+                             ),
+                           
+                             column(4,
+                                    selectInput("County",
+                                                "County:",
+                                                c("All",
+                                                  unique(hrcctable$County)))
+                             ),
+                         ),
+                         DT::dataTableOutput("table")
+                ),
                 
-                tabItem("Map",
-                        fluidRow(
-                            column(4,
-                                   sliderInput("Year3", "Declaration Year: ", min=2010, max=2017, value=c(2010, 2017), sep=""),
-                                   sliderInput("Year4", "Obligated Year: ", min=2010, max=2017, value=c(2010, 2017), sep="")
-                            ),
-                            column(4,
-                                   selectInput("Incident2",
-                                               "Incident Type:",
-                                               c("All",
-                                                 unique(hrcc$incidentType))),
-                                   selectInput("State2",
-                                               "State:",
-                                               c("All",
-                                                 unique(hrcc$state)))
-                            ),
-                            column(4,
-                                   radioButtons("Amount", "Select a view", c("Project Amount","Federal Share Obligated", "Total Obligated")),
-                            ),
-                        ),
-                        leafletOutput("LeafletPlot")
-                
+            tabItem("Map",
+                         
+                         fluidRow(
+                             column(4,
+                                    sliderInput("Year2", "Declaration Year:", min=2009, max=2018, value=c(2009, 2018)),
+                                    sliderInput("Year4",
+                                                "Obligated Year:",
+                                                min=2009, max=2018, value=c(min(hrcc$obligatedYear), max(hrcc$obligatedYear)))
+                                    
+                             ),
+                             column(4,
+                                    selectInput("Incident2",
+                                                "Incident Type:",
+                                                c("All",
+                                                  unique(hrcc$incidentType)))
+                             ),
+                             column(4,
+                                    selectInput("State2",
+                                                "State:",
+                                                c("All",
+                                                  unique(hrcc$state)))
+                             ),
+                             column(4,
+                                    radioButtons("Amount",
+                                                "Select a View:",
+                                                c("ProjectAmount","FederalShareObligated", "ToatlObligated"))
+                             ),
+                         ),
+                         leafletOutput("LeafletPlot")
+                )
             )
         )
-    )
 )
 
 server <- function(input, output) {
     
     output$table <- DT::renderDataTable(DT::datatable({
         data <- hrcctable
-        data <- filter(data,between(data$DeclarationYear,input$Year1[1],input$Year1[2]))
-        data <- filter(data,between(data$ObligatedYear,input$Year2[1],input$Year2[2]))
+        data <- filter(data,Declaration_Year >= input$Year[1] & Declaration_Year <= input$Year[2])
+        data <- filter(data,Obligated_Year >= input$Year3[1] & Obligated_Year <= input$Year3[2])
         if (input$DisasterNum != "All") {
             data <- data[data$DisasterNum == input$DisasterNum,]
         }
-        if (input$Incident1 != "All") {
-            data <- data[data$Incident == input$Incident1,]
+        # if (input$Year != "All") {
+        #     data <- data[data$Year == input$Year,]
+        # }
+        if (input$Incident != "All") {
+            data <- data[data$Incident == input$Incident,]
         }
         if (input$State != "All") {
             data <- data[data$State == input$State,]
@@ -120,34 +141,34 @@ server <- function(input, output) {
     output$LeafletPlot <- renderLeaflet({
         data <- hrcc
         loadJ <- loadJ
-        data <- filter(data,between(data$declarationYear,input$Year3[1],input$Year3[2]))
-        data <- filter(data,between(data$obligatedYear,input$Year4[1],input$Year4[2]))
+        # if (input$Year2 != "All") {
+        #     data <- data[data$declarationYear == input$Year2,]
+        # }
+        data <- filter(data,declarationYear >= input$Year2[1] & declarationYear <= input$Year2[2])
+        data <- filter(data,obligatedYear >= input$Year4[1] & obligatedYear <= input$Year4[2])
         if (input$Incident2 != "All") {
             data <- data[data$incidentType == input$Incident2,]
         }
         if (input$State2 != "All") {
             data <- data[data$state == input$State2,]
         }
-        
+       
         dataplot <- data %>% group_by(Fips)
-        if (input$Amount == "Project Amount"){
+        if (input$Amount == "ProjectAmount"){
             dataJ <-dataplot %>% 
-                summarise(state = unique(state), county = unique(county), 
-                          PlotAmount = sum(projectAmount)/1000,
-                          .groups = "drop")
+                    summarise(state = unique(state), county = unique(county), 
+                          PlotAmount = sum(projectAmount)/1000)
             
         }
-        else if (input$Amount == "Federal Share Obligated"){
+        else if (input$Amount == "FederalShareObligated"){
             dataJ <-dataplot %>% 
                 summarise(state = unique(state), county = unique(county), 
-                          PlotAmount = sum(federalShareObligated)/1000,
-                          .groups = "drop")
+                          PlotAmount = sum(federalShareObligated)/1000)
         }
-        else if (input$Amount == "Total Obligated"){
+        else if (input$Amount == "ToatlObligated"){
             dataJ <-dataplot %>% 
                 summarise(state = unique(state), county = unique(county), 
-                          PlotAmount = sum(totalObligated)/1000,
-                          .groups = "drop")
+                          PlotAmount = sum(totalObligated)/1000)
         }
         
         dataJ <- dataJ %>% rename(GEO_ID = Fips)
@@ -164,22 +185,21 @@ server <- function(input, output) {
                           round(dataJoin$PlotAmount,2) ,
                           " ","thousands") 
         LeafletMap <- leaflet() %>% 
-            addProviderTiles("CartoDB.Positron") %>%
-            addProviderTiles(providers$Stamen.TonerLines,
-                             options = providerTileOptions(opacity = 0.75)) %>%
-            setView(-89.275673, 37.098, zoom = 4) %>%
-            setMaxBounds(lng1=-130, lng2=-60, lat1=23, lat2=50) %>%
-            addPolygons(data = dataJoin,
-                        fillColor = ~pal(PlotAmount), 
-                        color = "#BDBDC3",
-                        fillOpacity  = 0.5, 
-                        smoothFactor = 0.2,
-                        weight = 1,
-                        popup = i_popup) %>%
-            addLegend(pal = pal,
-                      values = dataJoin$PlotAmount,
-                      position="bottomright",
-                      title = paste0("Amount in thousands"))
+                        addProviderTiles("CartoDB.Positron") %>%
+                        addProviderTiles(providers$Stamen.TonerLines,
+                                         options = providerTileOptions(opacity = 0.75)) %>%
+                        setView(-89.275673, 37.098, zoom = 4) %>%
+                        addPolygons(data = dataJoin,
+                                    fillColor = ~pal(PlotAmount), 
+                                    color = "#BDBDC3",
+                                    fillOpacity  = 0.5, 
+                                    smoothFactor = 0.2,
+                                    weight = 1,
+                                    popup = i_popup) %>%
+                        addLegend(pal = pal,
+                                  values = dataJoin$PlotAmount,
+                                  position="bottomright",
+                                  title = paste0("Amount in thousands"))
         LeafletMap
     }
     )
