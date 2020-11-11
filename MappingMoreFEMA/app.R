138a1026a7e3ab9d840f851beae177e6f7dc0390
Danping Liu <danping.liu@outlook.com>
2020-11-11 15:43:43 +0800

    change to shiny dashboard

--- MappingMoreFEMA/app.R
+++ MappingMoreFEMA/app.R
@@ -4,8 +4,6 @@ library(tidyverse)
 library(magrittr)
 library(drat)
 library(maps)
-library(ggplot2)
-library(dplyr)
 library(rgdal)
 library(geojsonio)
 library(tigris)
@@ -14,6 +12,7 @@ library(leaflet)
 library(grid)
 library(kableExtra)
 library(gridExtra)
+library(shinydashboard)
 
 hrcc <- read.csv("hrc_county.csv", head = TRUE)
 hrcctable <- hrcc[,c(1:3,7,9:12)]
@@ -23,83 +22,75 @@ colnames(hrcctable) <-  c("DisasterNum","Year","Incident","State","County","Proj
 loadJ <- geojsonio::geojson_read("gz_2010_us_050_00_5m.json", what = "sp")
 loadJ$GEO_ID <- loadJ$GEO_ID %>% substr(start = 10, stop = 14)
 
-
-ui <- fluidPage(
-    
-    titlePanel("MappingFEMA"),
-    
-    sidebarLayout(
-        tabsetPanel(
-            conditionalPanel(
-                'input.dataset === "hrcctable"'),
-            
-            conditionalPanel(
-                'input.dataset === "hrcc" ',
+ui <- dashboardPage(
+        dashboardHeader(title="FEMA Mapping"),
+        dashboardSidebar(
+            sidebarMenu(
+                menuItem("Table", tabName="Table"),
+                menuItem("Map", tabName="Map")
             )
         ),
-        
-        mainPanel(
-            tabsetPanel(
-                id = 'dataset',
-                tabPanel("Hurricane and Storms Data",
-                         fluidRow(
-                             column(4,
-                                    selectInput("DisasterNum",
-                                                "DisasterNum: ",
-                                                c("All",
-                                                  unique(hrcctable$DisasterNum)))
-                             ),
-                             column(4,
-                                    sliderInput("Year", "Year: ", min=2010, max=2017, value=c(2010, 2017))
-                             ),
-                             column(4,
-                                    selectInput("Incident",
-                                                "Incident Type:",
-                                                c("All",
-                                                  unique(hrcctable$Incident)))
-                             ),
-                             column(4,
-                                    selectInput("State",
-                                                "State:",
-                                                c("All",
-                                                  unique(hrcctable$State)))
-                             ),
-                             column(4,
-                                    selectInput("County",
-                                                "County:",
-                                                c("All",
-                                                  unique(hrcctable$County)))
-                             ),
-                         ),
-                         DT::dataTableOutput("table")
+        dashboardBody(
+            tabItems(
+                tabItem("Table",
+                        fluidRow(
+                            column(4,
+                                   sliderInput("Year", "Year: ", min=2010, max=2017, value=c(2010, 2017))
+                            ),
+                            
+                            column(4,
+                                   selectInput("DisasterNum",
+                                               "DisasterNum: ",
+                                               c("All",
+                                                 unique(hrcctable$DisasterNum)))
+                            ),
+                            column(4,
+                                   selectInput("Incident",
+                                               "Incident Type:",
+                                               c("All",
+                                                 unique(hrcctable$Incident)))
+                            ),
+                            column(4,
+                                   selectInput("State",
+                                               "State:",
+                                               c("All",
+                                                 unique(hrcctable$State)))
+                            ),
+                            column(4,
+                                   selectInput("County",
+                                               "County:",
+                                               c("All",
+                                                 unique(hrcctable$County)))
+                            ),
+                        ),
+                        DT::dataTableOutput("table")
                 ),
                 
-                tabPanel("LeafletPlot",
-                         
-                         fluidRow(
-                             column(4,
-                                    sliderInput("Year2", "Year: ", min=2010, max=2017, value=c(2010, 2017))
-                             ),
-                             column(4,
-                                    selectInput("Incident2",
-                                                "Incident Type:",
-                                                c("All",
-                                                  unique(hrcc$incidentType)))
-                             ),
-                             column(4,
-                                    selectInput("State2",
-                                                "State:",
-                                                c("All",
-                                                  unique(hrcc$state)))
-                             ),
-                             column(4,
-                                    selectInput("Amount",
-                                                "Select a View:",
-                                                c("ProjectAmount","FederalShareObligated", "ToatlObligated"))
-                             ),
-                         ),
-                         leafletOutput("LeafletPlot"),
-                )
+                tabItem("Map",
+                        fluidRow(
+                            column(4,
+                                   sliderInput("Year2", "Year: ", min=2010, max=2017, value=c(2010, 2017))
+                            ),
+                            column(4,
+                                   selectInput("Incident2",
+                                               "Incident Type:",
+                                               c("All",
+                                                 unique(hrcc$incidentType)))
+                            ),
+                            column(4,
+                                   selectInput("State2",
+                                               "State:",
+                                               c("All",
+                                                 unique(hrcc$state)))
+                            ),
+                            column(4,
+                                   selectInput("Amount",
+                                               "Select a View:",
+                                               c("ProjectAmount","FederalShareObligated", "ToatlObligated"))
+                            ),
+                        ),
+                        leafletOutput("LeafletPlot")
+                
             )
         )
     )
@@ -142,11 +133,11 @@ server <- function(input, output) {
         if (input$State2 != "All") {
             data <- data[data$state == input$State2,]
         }
-       
+        
         dataplot <- data %>% group_by(Fips)
         if (input$Amount == "ProjectAmount"){
             dataJ <-dataplot %>% 
-                    summarise(state = unique(state), county = unique(county), 
+                summarise(state = unique(state), county = unique(county), 
                           PlotAmount = sum(projectAmount)/1000)
             
         }
@@ -175,21 +166,21 @@ server <- function(input, output) {
                           round(dataJoin$PlotAmount,2) ,
                           " ","thousands") 
         LeafletMap <- leaflet() %>% 
-                        addProviderTiles("CartoDB.Positron") %>%
-                        addProviderTiles(providers$Stamen.TonerLines,
-                                         options = providerTileOptions(opacity = 0.75)) %>%
-                        setView(-89.275673, 37.098, zoom = 4) %>%
-                        addPolygons(data = dataJoin,
-                                    fillColor = ~pal(PlotAmount), 
-                                    color = "#BDBDC3",
-                                    fillOpacity  = 0.5, 
-                                    smoothFactor = 0.2,
-                                    weight = 1,
-                                    popup = i_popup) %>%
-                        addLegend(pal = pal,
-                                  values = dataJoin$PlotAmount,
-                                  position="bottomright",
-                                  title = paste0("Amount in thousands"))
+            addProviderTiles("CartoDB.Positron") %>%
+            addProviderTiles(providers$Stamen.TonerLines,
+                             options = providerTileOptions(opacity = 0.75)) %>%
+            setView(-89.275673, 37.098, zoom = 4) %>%
+            addPolygons(data = dataJoin,
+                        fillColor = ~pal(PlotAmount), 
+                        color = "#BDBDC3",
+                        fillOpacity  = 0.5, 
+                        smoothFactor = 0.2,
+                        weight = 1,
+                        popup = i_popup) %>%
+            addLegend(pal = pal,
+                      values = dataJoin$PlotAmount,
+                      position="bottomright",
+                      title = paste0("Amount in thousands"))
         LeafletMap
     }
     )
